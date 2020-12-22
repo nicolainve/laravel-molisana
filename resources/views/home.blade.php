df273397a58bdf585eb091c1f3684e7dbf3168e7
Nicola <nginvernizzi1@gmail.com>
2020-12-22 16:42:52 +0100

    categorizes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,13 @@
 @extends('layouts.main')
 
 @section('content')
-    
-<section class="container">
+
+@if (! empty($corte))
+<section class="type container">
+    <h2>LE LUNGHE</h2>
+
     <div class="cards">
-        @foreach($cards as $card)
+        @foreach($lunghe as $card)
             <div class="card">
                 <img src="{{ $card['src'] }}" alt="{{ $card['titolo'] }}">
                 <h3>{{ $card['titolo'] }}</h3>
@@ -13,5 +16,38 @@
         @endforeach
     </div>
 </section>
+@endif
+
+@if (! empty($corte))
+<section class="type container">
+    <h2>LE CORTE</h2>
+
+    <div class="cards">
+        @foreach($corte as $card)
+            <div class="card">
+                <img src="{{ $card['src'] }}" alt="{{ $card['titolo'] }}">
+                <h3>{{ $card['titolo'] }}</h3>
+                <a href="#">Vedi prodotto</a>
+            </div>
+        @endforeach
+    </div>
+</section>
+@endif
+
+@if (! empty($corte))
+<section class="type container">
+    <h2>LE CORTISSIME</h2>
+
+    <div class="cards">
+        @foreach($cortissime as $card)
+            <div class="card">
+                <img src="{{ $card['src'] }}" alt="{{ $card['titolo'] }}">
+                <h3>{{ $card['titolo'] }}</h3>
+                <a href="#">Vedi prodotto</a>
+            </div>
+        @endforeach
+    </div>
+</section>
+@endif
 
 @endsection
